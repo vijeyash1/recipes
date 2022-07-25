2137cefca38f95a4cbef5d9385f71590e8829657
vijeyash <vijeyash@gmail.com>
2022-07-25 15:25:05 +0530

    biolerplate

--- main.go
+++ main.go
@@ -4,13 +4,14 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	router := gin.Default()
-	router.GET("/", response)
+	router.GET("/:name", response)
 
 	router.Run(":8080") // listen and serve on
 }
 
 func response(c *gin.Context) {
+	name := c.Param("name")
 	c.JSON(200, gin.H{
-		"message": "response from server",
+		"message": "Hello " + name,
 	})
 }
